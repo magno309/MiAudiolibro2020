8ea64f9fa0797c0726889b3b814d2be3bff286d1
magno309 <48113838+magno309@users.noreply.github.com>
2021-02-08 14:39:24 -0600

    Agregado servicios

--- app/src/main/java/com/example/miaudiolibro2020/DetalleFragment.java
+++ app/src/main/java/com/example/miaudiolibro2020/DetalleFragment.java
@@ -1,24 +1,36 @@
 package com.example.miaudiolibro2020;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class DetalleFragment extends Fragment {
+import com.example.miaudiolibro2020.services.MiServicio;
+
+import java.io.IOException;
+
+public class DetalleFragment extends Fragment implements View.OnTouchListener, MediaPlayer.OnPreparedListener, MediaController.MediaPlayerControl {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
     public static final String ARG_ID_LIBRO = "id_libro";
+
+    //Reproductores
+    private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -76,11 +88,108 @@ public class DetalleFragment extends Fragment {
     }
 
     private void ponInfoLibro(int id, View vista){
+        Intent servicio = new Intent(getContext(), MiServicio.class);
+        getActivity().startService(servicio);
         Libro libro = Libro.ejemploLibros().elementAt(id);
         ((TextView) vista.findViewById(R.id.titulo)).setText(libro.titulo);
         ((TextView) vista.findViewById(R.id.autor)).setText(libro.autor);
         ((ImageView) vista.findViewById(R.id.portada)).setImageResource(libro.recursoImagen);
+        vista.setOnTouchListener(this);
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+        }
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnPreparedListener(this);
+        mediaController = new MediaController(getActivity());
+        Uri audio = Uri.parse(libro.urlAudio);
 
+
+        try {
+            mediaPlayer.setDataSource(getActivity(), audio);
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mediaPlayer.start();
+        mediaController.setMediaPlayer(this);
+        mediaController.setAnchorView(getView().findViewById(R.id.detalle_fragment));
+        mediaController.setEnabled(true);
+        mediaController.show();
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void start() {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int i) {
+        mediaPlayer.seekTo(i);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return mediaPlayer.getAudioSessionId();
+    }
+
+    @Override
+    public void onStop(){
+        mediaController.hide();
+        try {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+        super.onStop();
+    }
 }
---
+++ app/src/main/java/com/example/miaudiolibro2020/services/MiServicio.java
@@ -0,0 +1,37 @@
+package com.example.miaudiolibro2020.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MiServicio extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MSAL", "Servicio creado");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        //Tarea exhaustiva
+        Log.d("MSAL", "Iniciando tarea pesada");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d("MSAL", "Iniciando tarea pesada");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+}
